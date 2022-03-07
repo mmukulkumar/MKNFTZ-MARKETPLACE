02bb4d86310abbeb58edaddb382bffd553b25dff
asmaa <mmukulkumar@live.com>
2022-03-07 19:10:23 +0000

    MKNFTZ_MARKETPLACE UPDATES

---
+++ migrations/2_MKNFTToken.js
@@ -0,0 +1,10 @@
+const MKNFTToken = artifacts.require("MKNFTToken");
+
+module.exports = function (deployer) {
+  deployer.deploy(MKNFTToken);
+};
+
+
+
+
+//token name = MKNFTToken //
---
+++ migrations/3_mknftz-mp.js
@@ -0,0 +1,7 @@
+const MKNFTZ_MARKETPLACE = artifacts.require("MKNFTZ_MARKETPLACE");
+
+module.exports = function (deployer) {
+  deployer.deploy(MKNFTZ_MARKETPLACE);
+};
+
+// CONTARCT NAME =  MKNFTZ_MARKETPLACE
